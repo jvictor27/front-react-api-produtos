bee8d92282de50a8b89c626d4c3af73f6f06a49d
jvictor27 <jvictor451@gmail.com>
2020-10-12 14:18:19 -0300

    Ajuste grid

--- src/common/layout/grid.jsx
+++ src/common/layout/grid.jsx
@@ -1,26 +1,28 @@
-import React, ( Component ) from 'react'
+ 
+import React, { Component } from 'react'
 
 class Grid extends Component {
+
     toCssClasses(numbers) {
         const cols = numbers ? numbers.split(',') : []
         let classes = ''
 
-        if (cols[0]) classes += `col-xs-${cols[0]}`
-        if (cols[1]) classes += `col-sm-${cols[1]}`
-        if (cols[2]) classes += `col-md-${cols[2]}`
-        if (cols[3]) classes += `col-lg-${cols[3]}`
+        if(cols[0]) classes += `col-xs-${cols[0]}` 
+        if(cols[1]) classes += ` col-sm-${cols[1]}`
+        if(cols[2]) classes += ` col-md-${cols[2]}`
+        if(cols[3]) classes += ` col-lg-${cols[3]}`
 
-        return classes;
+        return classes 
     }
 
     render() {
-        const gridClasses = this.toCssClasses(this.props.clos || '12')
+        const gridClasses = this.toCssClasses(this.props.cols || '12')
         return (
-            <div className={gridClasses}>
-                {this.render.children}
-            </div>
+            <div className={gridClasses}> 
+                {this.props.children}
+            </div> 
         )
     }
 }
 
-default export Grid
+export default Grid
--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
 
 class Dashboard extends Component {
     render() {
@@ -9,7 +10,12 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title="Dashboard" subTitle='Versão 1.0' />
                 <Content>
-                    Dashboard
+                    <ValueBox cols='12,4' color='white' icon='fa fa-credit-card'
+                        value='R$2000' text='Total investido' />
+                    <ValueBox cols='12,4' color='blue' icon='fa fa-money'
+                        value='R$5000' text='Total de vendas' />
+                    <ValueBox cols='12,4' color='green' icon='fa fa-bank'
+                        value='R$3000' text='Total de lucro' />     
                 </Content>
             </div>
         )
