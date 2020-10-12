ab04c6a4c0d05465fff2caa1f945540943798682
jvictor27 <jvictor451@gmail.com>
2020-10-12 12:05:12 -0300

    Navegação com React-router

---
+++ src/main/routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Router, Route, Redirect, hashHistory } from 'react-router'
+
+import Dashboard from '../dashboard/dashboards'
+import Categoria from '../categoria/categoria'
+import TipoOpcao from '../tipoOpcao/tipoOpcao'
+import Opcao from '../opcao/opcao'
+import Produto from '../produto/produto'
+
+export default props => (
+    <Router history={hashHistory}>
+        <Route path='/' component={Dashboard} />
+        <Route path='/categorias' component={Categoria} />
+        <Route path='/tiposOpcoes' component={TipoOpcao} />
+        <Route path='/opcoes' component={Opcao} />
+        <Route path='/produtos' component={Produto} />
+        <Redirect from='*' to='/' />
+    </Router>
+)
---
+++ src/produto/produto.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <div>
+        <h1>Produtos</h1>
+    </div>
+)
